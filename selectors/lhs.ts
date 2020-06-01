055f07cc7581b99e4162da80a9ebadd81ca21004
nhutthanh340 <nhutthanh340@gmail.com>
2020-06-01 21:49:20 +0700

    deploy

--- selectors/lhs.ts
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
-// See LICENSE.txt for license information.
-
-import {GlobalState} from 'types/store';
-
-export function getIsLhsOpen(state: GlobalState): boolean {
-    return state.views.lhs.isOpen;
-}
